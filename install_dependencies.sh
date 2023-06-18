211193cbd20e0a2b52ab449a16dbeabc8715c330
sam <martinsa0182@gmail.com>
2023-06-18 10:32:08 +0100

    installed spdlog from source to work across distros and added arm64 config to travis

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sudo apt-get -y install git cmake build-essential libglib2.0-dev python3-dev valgrind libspdlog-dev
+sudo apt-get -y install git cmake build-essential libglib2.0-dev python3-dev valgrind
 
 sudo rm -rf dependencies
 mkdir dependencies && cd dependencies || exit
@@ -14,6 +14,16 @@ cd ..
 
 git clone https://github.com/lcm-proj/lcm
 cd lcm || exit
+mkdir build && cd build || exit
+cmake ..
+make
+sudo make install
+cd ..
+
+
+#we could use sudo apt-get install libspdlog-dev for debian, however it's more portable to build from source
+git clone https://github.com/gabime/spdlog
+cd spdlog || exit
 mkdir build && cd build || exit
 cmake ..
 make
