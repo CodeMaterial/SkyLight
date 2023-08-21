e346bb85e4f7460960987e3053c4f86c5e7fddf8
sam <martinsa0182@gmail.com>
2023-08-21 20:27:52 +0100

    Annoyingly had to merge button and spi into one executable as the pigpio system requires a file lock.

--- src/nodes/gpio/main.cpp
+++ src/nodes/gpio/main.cpp
@@ -1,14 +1,13 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <chrono>
 #include <thread>
 #include <pigpio.h>
 #include "spdlog/spdlog.h"
 
 #include "skylight_messaging.h"
 #include "skylight_config.h"
+#include "skylight_time.h"
 
 #include "skylight_message/pixel_buffer.hpp"
+#include "skylight_message/trigger.hpp"
 
 
 class BufferHandler
@@ -31,7 +30,7 @@ public:
     void LCMBufferUpdate(const lcm::ReceiveBuffer *rbuf,
                      const std::string &chan,
                      const skylight_message::pixel_buffer *msg) {
-        spdlog::debug("buffer received on {} at {}", chan, msg->timestamp);
+        spdlog::info("buffer received on {} at {}", chan, msg->timestamp);
 
         char* charBuffer = const_cast<char*>(reinterpret_cast<const char*>(msg->buffer));
 
@@ -42,6 +41,16 @@ private:
     int mSpiDevice;
 
 };
+
+
+void ButtonCallback(int gpioPin, int level, unsigned int tick, void* messaging)
+{
+    skylight_message::trigger button_press;
+    button_press.timestamp = skylight::Now();
+    std::string channel =  fmt::format("button_{}_{}", gpioPin, level?"up":"down");
+    static_cast<skylight::Messaging*>(messaging)->publish(channel, &button_press);
+}
+
 
 int main(int argc, char **argv) {
 
@@ -58,7 +67,7 @@ int main(int argc, char **argv) {
         return 1;
     }
 
-    std::shared_ptr<toml::Table> pConfig = skylight::GetConfig("skylight_led_driver.toml");
+    std::shared_ptr<toml::Table> pConfig = skylight::GetConfig("skylight_gpio.toml");
 
     if(!pConfig)
     {
@@ -84,6 +93,31 @@ int main(int argc, char **argv) {
 
     lcm::Subscription* sub = messaging.subscribe("led/buffer", &BufferHandler::LCMBufferUpdate, &bufferHandler);
     sub->setQueueCapacity(1);
+
+
+    std::vector<long int> pGPIO_ports = *pConfig->getArray("GPIO_ports")->getIntVector();
+    auto [debounceOk, debounce] = pConfig->getInt("debounce");
+
+
+    if(!debounceOk)
+    {
+        spdlog::error("button system cannot load the debounce value");
+        return 1;
+    }
+    if(pGPIO_ports.empty())
+    {
+        spdlog::error("button system doesn't have any configured GPIO ports");
+        return 1;
+    }
+
+    for(long int GPIO_port: pGPIO_ports)
+    {
+        spdlog::info("button system setting up gpio port {} with debounce of {}", GPIO_port, debounce);
+        gpioSetMode(GPIO_port, PI_INPUT);
+        gpioSetPullUpDown(GPIO_port, PI_PUD_UP);
+        gpioGlitchFilter(GPIO_port, debounce);
+        gpioSetAlertFuncEx(GPIO_port, &ButtonCallback, static_cast<void*>(&messaging));
+    }
 
     while (0 == messaging.handle());
 
